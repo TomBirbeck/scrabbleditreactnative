7d8cbe98963b8afaefbecbea8be3223800fb8b9a
unknown <tombirbeck47@gmail.com>
2023-01-24 15:45:21 +0000

    header text set and styled

---
+++ components/Header.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, Text, View} from 'react-native';
+
+const Header: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.bannerContainer}>
+          <Text style={styles.bannerText}>Scrabbledit</Text>
+          </View>
+          <Text style={styles.motto}>We can't trust Barry with the scores again</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    //   backgroundColor: '#26584C',
+      alignItems: 'center',
+      // justifyContent: 'center',
+    },
+    bannerContainer: {
+      backgroundColor: '#E53E48',
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      borderRadius: 5,
+      marginTop: 4,
+    },
+    bannerText: {
+      color: 'white',
+      fontSize: 25,
+      fontWeight: 'bold'
+    },
+    motto: {
+      color: 'white',
+      fontWeight: 'bold',
+      marginTop: 5,
+    }
+  });
+
+  export default Header
