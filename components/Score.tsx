be2052e490ecebc047dd45a7b43a4ea285f9c48e
unknown <tombirbeck47@gmail.com>
2023-01-26 01:29:55 +0000

    added placeholde text, changed colour to white and styled check word button

--- components/Score.tsx
+++ components/Score.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useCallback, useEffect} from'react'
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Button } from 'react-native'
 import { calculateScrabbleScore } from '../utilities/scoreCalc'
 
 interface ScoreProps {
@@ -11,7 +11,6 @@ const Score = ({setContext}: ScoreProps) => {
     const [score, setScore] = useState(0)
     const [doubles, setDoubles] = useState('')
     const [triples, setTriples] = useState('')
-    const [check, setCheck] = useState<any>()
 
     useEffect(()=>{
         setContext(word.trim().toUpperCase().split(''))
@@ -19,7 +18,6 @@ const Score = ({setContext}: ScoreProps) => {
 
     const handleSubmit = useCallback((word: string, doubles: string, triples: string) => {
         const wordSplit = word.trim().toUpperCase().split('')
-        setCheck(wordSplit)
         const doublesSplit = doubles.trim().toUpperCase().split('')
         const triplesSplit = triples.trim().toUpperCase().split('')
         const score = calculateScrabbleScore({word:wordSplit, doubles: doublesSplit, triples: triplesSplit})
@@ -27,29 +25,37 @@ const Score = ({setContext}: ScoreProps) => {
     },[word, doubles, triples])
     return (
         <View style={styles.container}>
-            <TextInput style={styles.textInput} value={word} onChangeText={setWord}/>
-            <TextInput style={styles.textInput}  value={doubles} onChangeText={setDoubles}/>
-            <TextInput style={styles.textInput}  value={triples} onChangeText={setTriples}/>
-            <TouchableOpacity onPress={()=>handleSubmit(word, doubles, triples)}>
-                <Text style={styles.score}>Check Score</Text>
+            <TouchableOpacity style={styles.button} onPress={()=>handleSubmit(word, doubles, triples)}>
+            <Text style={styles.score}>Check Word</Text>
             </TouchableOpacity>
             <Text style={styles.score}>Score: {score}</Text>
-            <Text style={styles.score}>check: {check}</Text>
+            <TextInput style={styles.textInput} value={word} onChangeText={setWord} placeholder='Enter your word' placeholderTextColor='white'/>
+            <TextInput style={styles.textInput}  value={doubles} onChangeText={setDoubles} placeholder='Enter any double letters' placeholderTextColor='white'/>
+            <TextInput style={styles.textInput}  value={triples} onChangeText={setTriples} placeholder='Enter any triple letters' placeholderTextColor='white'/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 3,
         width: 300,
     },
     textInput: {
-        borderWidth: 1,
-        borderColor: 'yellow',
+        borderWidth: 5,
+        color: 'white',
+        borderColor: '#DBB684',
         padding: 10,
         borderRadius: 5,
         marginBottom: 10,
+    },
+    button: {
+        backgroundColor: '#3A9366',
+        padding: 3,
+        width: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
     },
     score: {
         color: 'white'
