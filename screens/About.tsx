572fc7ab84114544624f8f9fbcd525c7bbc6bd3c
unknown <tombirbeck47@gmail.com>
2023-01-30 16:20:39 +0000

    navigation added to about page

---
+++ screens/About.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+const About = () => {
+    return(
+        <View>
+            <Text>About the app</Text>
+        </View>
+    )
+}
+
+export default About
